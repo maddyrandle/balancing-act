0fe9f9959f326b8d8908ccdce41413c7c40a7ee3
Madison Randle <madisonrandle25@gmail.com>
2019-11-23 09:28:33 -1000

    Remove blue button outline when clicked

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var closeIcon = document.querySelector(".close-icon");
 var greeting = document.querySelector(".welcome-message");
+var clipboardIcon = document.querySelector(".btn-icon");
 
 greeting.addEventListener("click", removeBanner);
+clipboardIcon.addEventListener("click", switchLightBorder);
 
 function removeBanner() {
   greeting.remove();
